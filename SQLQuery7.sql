5f52ac9b254addcb2bdc4acce0d7b489c35cff64
jumel-15 <fernandezjumel8@gmail.com>
2025-05-28 09:28:46 +0800

    Delete SQLQuery7.sql

--- SQLQuery7.sql
+++
@@ -1,4 +0,0 @@
-SELECT Company_Name, Model_Name, Back_Camera, Launched_Price_USA_USD FROM ['Mobiles Dataset (2025)$']
-WHERE Company_Name = 'Samsung'
-AND Back_Camera LIKE '%50MP%'
-ORDER BY Launched_Price_USA_USD DESC
