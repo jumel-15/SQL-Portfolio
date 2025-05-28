79ddf90c4b29649e00909bff49268ee7794cf90e
jumel-15 <fernandezjumel8@gmail.com>
2025-05-28 09:28:56 +0800

    Delete SQLQuery6.sql

--- SQLQuery6.sql
+++
@@ -1,4 +0,0 @@
-SELECT Company_Name, Model_Name, Processor, Launched_Price_USA_USD, [Launched Year] FROM ['Mobiles Dataset (2025)$']
-WHERE Company_Name = 'Apple'
-AND Processor LIKE '%A17%'
-ORDER BY Launched_Price_USA_USD DESC
